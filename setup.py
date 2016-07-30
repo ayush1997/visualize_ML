e83f2e43d8430eee27f4bba780a5724f450bf9b3
ayush <ayushkumarsingh97@gmail.com>
2016-07-30 20:21:25 +0530

    changes

---
+++ setup.py
@@ -0,0 +1,90 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='visualize_ML',
+
+    version='0.1.1',
+
+    description='To visualize various processes involved in dealing with a Machine Learning problem.',
+    long_description='Helps to visualize processes such as Data Exploration and Predictive Models through different statistical graphs while dealing with a ML problem.',
+
+    # The project's main homepage.
+    url='https://github.com/ayush1997/visualize_ML',
+
+
+    author='ayush1997',
+    author_email='ayushkumarsingh97@gmail.com',
+
+
+    license='MIT',
+
+
+    classifiers=[
+
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Data Scientist',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='visualization MachineLearning DataScience',
+
+    packages=find_packages(exclude=[]),
+
+
+    install_requires=['scipy==0.14.1',"scikit-learn==0.17.1","pandas==0.18.1","numpy==1.11.1","matplotlib==1.4.2"],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={
+        'sample': ['package_data.dat'],
+    },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[('my_data', ['data/data_file'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'sample=sample:main',
+        ],
+    },
+)
